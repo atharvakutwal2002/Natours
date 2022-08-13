e3f5e66fb9e678b848825c370dc89ae825d84ec8
Atharva <atharvakutwal2002@gmail.com>
2022-08-13 22:58:36 +0530

    middlewares chained

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,6 +15,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).send({
     status: 'success',
@@ -28,7 +38,7 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
- 
+
   res.status(200).send({
     status: 'success',
     tour,
@@ -54,7 +64,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  
   res.status(200).json({
     status: 'success',
     data: {
