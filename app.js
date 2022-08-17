fe4ff1c5554ddbec3b60c92833826dec987d76a3
Atharva <atharvakutwal2002@gmail.com>
2022-08-17 21:42:35 +0530

    handled unhandled routes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const userRouter = require('./routes/userRoutes');
 
 app.use(express.json());
 
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 //middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -16,5 +16,12 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/tours', tourRouter);
 
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
