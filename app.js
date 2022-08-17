2c3db5ec1983d4a26cff80bb2a528ba61f25c52f
Atharva <atharvakutwal2002@gmail.com>
2022-08-17 23:12:56 +0530

    error controller added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const morgan = require('morgan');
 const app = express();
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -18,10 +20,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `can't find ${req.originalUrl} on this server!`,
-  });
+  next(new AppError(`cant find ${req.originalUrl} on this server`), 404);
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
