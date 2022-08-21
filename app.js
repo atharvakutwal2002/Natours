88d6802fd6f5a6d624482142716b2761f2964592
Atharva <atharvakutwal2002@gmail.com>
2022-08-21 17:36:43 +0530

    added middlewares

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -35,6 +37,19 @@ app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
 
 app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'prize',
+    ],
+  })
+);
 
 app.use(express.static(`${__dirname}/public`));
 
