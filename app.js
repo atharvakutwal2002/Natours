1a3b5a9022c780182f34df2abad916fba4a44002
Atharva <atharvakutwal2002@gmail.com>
2022-08-21 17:16:25 +0530

    security headers

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-app.use(express.json());
-
 console.log(process.env.NODE_ENV);
+
 //middleware
+
+app.use(helmet()); //set security http headers
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -24,6 +27,10 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 
